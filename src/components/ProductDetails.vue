249a9f7c59dd286a3899d6901a7a80cf40b3c8ea
Andy <andy@Andys-MacBook-Pro.local>
2025-12-01 12:16:42 -0500

    solution

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, computed } from 'vue'
+import socksGreenImage from '@/assets/images/socks_green.jpeg'
+import socksBlueImage from '@/assets/images/socks_blue.jpeg'
+
+const props = defineProps({
+  details: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="detail in details">{{ detail }}</li>
+  </ul>
+</template>
