85bb998f19bbfa512417ebc78f64e7f87867e8df
Abdulkhayum2k4 <abdulkhayoom2004@gmail.com>
2026-03-09 12:17:30 +0530

    save

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,105 @@
-<script setup>
-import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+<script>
+import { ref } from "vue";
+const instock = ref(true);
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-const inStock = true
+// export default {
+//   data() {
+//     return {
+//       count: 0,
+//     };
+//   },
+//   methods:{
+//     increase(){
+//       this.count++;
+//     }
+//   }
+// };
 
+// export default {
+//   data() {
+//     return {
+//       author: {
+//         name: "John Doe",
+//         books: ["vue1", "vue2", "vue3"],
+//       },
+//     };
+//   },
+//   computed: {
+//     message() {
+//       return this.author.books.length > 0 ? "yes" : "no";
+//     },
+//   },
+// };
+
+// export default {
+//   data() {
+//     return {
+//       firstName: "John",
+//       lastName: "Doe",
+//     };
+//   },
+//   computed: {
+//     fullName: {
+//       // getter
+//       get() {
+//         return this.firstName ;
+//       },
+//       // setter
+//       set(newValue) {
+//         // Note: we are using destructuring assignment syntax here.
+//         // [this.firstName, this.lastName] = newValue.split(" ");
+//         this.firstName = newValue
+//       },
+//     },
+//   },
+// };
+
+// export default {
+//   data() {
+//     return {
+//       count: 1,
+//     };
+//   },
+//   computed: {
+//     // This computed will return the value of count when it's less or equal to 3.
+//     // When count is >=4, the last value that fulfilled our condition will be returned
+//     // instead until count is less or equal to 3
+//     alwaysSmall(_, previous) {
+//       if (this.count <= 3) {
+//         return this.count;
+//       }
+
+//       return previous;
+//     },
+//   },
+// };
+export default {};
 </script>
-  
+
 <template>
-  <div class="nav-bar"></div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-      </div>
-    </div>
+  <!-- <button @click="increase">Count: {{ count }}</button>
+  <button @click="increase">Count: <span v-html="count"></span></button> -->
+
+  <!-- <p>published? <span>{{ message }}</span></p> -->
+
+  <!-- <input v-model="fullName" />
+  <p>{{ firstName }}</p> -->
+  <!-- <p>{{ lastName }}</p> -->
+
+  <!-- <div>
+    <h2>Count: {{ count }}</h2>
+    <h2>Always Small: {{ alwaysSmall }}</h2>
+
+    <button @click="count++">Increase</button>
+  </div> -->
+  <div>
+    <p v-if="instock">In stock</p>
+    <p v-else="instock">Out of stock</p>
   </div>
 </template>
+
+<!-- <style scoped>
+button {
+  font-weight: bold;
+}
+</style> -->
